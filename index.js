acf4c06ca9707c8c38270f104602269cc3c83f05
wcubasdeveloper <st_wtml@hotmail.com>
2021-08-22 02:45:34 -0500

    prueba

--- index.js
+++ index.js
@@ -19,125 +19,206 @@ let io = require('socket.io')(server, options);
 //
 let itemConductor = {
   idconductor : 0,
-
 };
 
 let usuarioseteado = "";
-let arrConductoresActivos = [];
+let ARR_CONDUCTORES_ACTIVOS = []; //aqui se guardaran los conductores activos con sus respectivos ids
 //
 io.on('connection', (socket) => {
   //
   var socketID = socket.id;
-  let id = socket.handshake.query.id;
-  console.log("socketID", socketID, 'id->', id);
+  let codigousuario =  0;
+  let tipousuario = socket.handshake.query.tipousuario;
+  let itemconductor = {
+    codconductor : 0,
+    idconnection : ''
+  };
+
+  console.log("<------------- se conectó ---------------------->");
+  console.log("[tipo usuario]", tipousuario);
+
+  if(tipousuario == 'conductor'){ //si el usuario que se conecta es conductor
+    codigousuario = socket.handshake.query.codconductor;
+
+    itemconductor.codconductor = codigousuario;
+    itemconductor.idconnection = socketID;
+    setSessionConductor(itemconductor);
+  }
+
+  if(tipousuario == 'pasajero'){ //si el usuario que se conecta es pasajero
+
+  }
+
+  console.log("[codigo usuario]", codigousuario);
+  console.log("[ID connection SOCKET]", socketID);
+  //console.log("[conductores activos]");
+  //console.log(getSessionConductores());
+
+  //console.log("socketID", socketID, 'id->', id);
   //
   // console.log("room--->", nameRoom);
 
-  io.to(socketID).emit('sendsocketid', { socketidclient :  socketID}); //enviando el idSOCKET al cliente que ingresó
+  //io.to(socketID).emit('sendsocketid', { socketidclient :  socketID}); //enviando el idSOCKET al cliente que ingresó
 
-
-  //
-  // let json__ = [{
-  //   apellidos : 'cubas alegria'
-  // }];
-  
-  // json__.push({ apellidos : 'espinza cordova' });
-  // let datasave = JSON.stringify(json__);
-  //
-  // fs.writeFile('myjsonfile_.json', datasave, 'utf8', function(){
-  //   console.log("hola");
-  // });
-  //
-  // io.emit('sendidclisocket', {user: socket.id, event: 'left'});   
   socket.on('disconnect', function(){
-    console.log("se desconecto");
-    io.emit('users-changed', {user: socket.username, event: 'left'});   
+    console.log("<---------- se desconectó --------------->");
+    var removioitem =  removeitemSessionConductor(socket.id)
+    console.log("[idconnectionwebsocket]", socket.id); 
+    console.log("[removio item]", removioitem);
   });
   
-  socket.on('solicitaviaje', (objetoPasajero) => { //aqui el pasajero solicita un viaje a os conductores
-    //console.log("solicitó viajee", objetoPasajero);
+
+
+  socket.on('solicitaviaje', (objetoPasajero) => { //aqui el pasajero solicita un viaje a los conductores
+
+    console.log("[pasajero solicita viaje]");
     var pasajero = JSON.parse(objetoPasajero);
-    io.emit('pasajerosolicitaviaje', {pasajero, createdAt: new Date()});    
+    io.emit('pasajerosolicitaviaje', {pasajero, createdAt: new Date()});  
+
   });
 
   socket.on('enviapropuesta', (objetoPropuesta) => { //aqui el conductor envia propuesta al pasajero
     //console.log("solicitó viajee", objetoPasajero);
     var propuesta = JSON.parse(objetoPropuesta);
-
+    //console.log("<-------------conductor envia propuesta ------------------>");
+    //console.log(propuesta);
     io.emit('recibepropuesta', {propuesta, createdAt: new Date()});    
   });
 
 
   socket.on('datatrackconductor', (objetoTrack) => { //aqui el conductor envia el tracking para todos los usuarios
-    //console.log("solicitó viajee", objetoPasajero);
+    console.log("<------- TRACK ---------------->");
     var datatrack = JSON.parse(objetoTrack);
+    console.log(datatrack);
+
     io.emit('recibirtrackconductor', {datatrack, createdAt: new Date()});    
   });
 
-  socket.on('pasajeroconfirma', (objpasajeroSolicitud) => { //aqui el conductor envia propuesta al pasajero
-    console.log("objpasajeroSolicitud", objpasajeroSolicitud);
-    //
-    var idSocketconductorAceptado = objpasajeroSolicitud.clientsocket;
-
-    //recorriendo los conductores que ofertaron
-    objpasajeroSolicitud.conductoresofertaron.forEach( (item) => {
-      
-        //element.product_desc = element.product_desc.substring(0,10);
-      let idsocketconductor = item.propuesta.idsocketconductor.socketidclient;
-      if(idsocketconductor != idSocketconductorAceptado){
-        io.to(idsocketconductor).emit('pasajerorechazo', {item, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
-      }
-      console.log("item->>",idsocketconductor)
-    });
-
-    // io.emit('pasajeroconfirmo', {objpasajeroSolicitud, createdAt: new Date()});    
-    io.to(idSocketconductorAceptado).emit('pasajeroconfirmo', {objpasajeroSolicitud, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
+  socket.on('testtrack', (testvalue) => { //aqui el conductor envia el tracking para todos los usuarios
+    console.log("data->", testvalue);
   });
 
+
+  socket.on('pasajeroconfirma', (objpasajeroSolicitud) => { //aqui el pasajero acepta propuesta de un conductor [version actual por codconductor]
+
+    console.log("<------ pasajero confirma --------------->");
+    //console.log("objpasajeroSolicitud", objpasajeroSolicitud.conductoresofertaron[0].propuesta);
+    var codconductor = objpasajeroSolicitud.conductoresofertaron[0].propuesta.idconductor;
+    var idSocketconductorAceptado = "";
+    var conductoresactivos = getSessionConductores();
+  
+
+    for(let i = 0 ; i < conductoresactivos.length ; i++){
+      var item = conductoresactivos[i];
+      var codigoconductor = conductoresactivos[i].codconductor;
+      var codigowsconnection = conductoresactivos[i].idconnection;
+      //
+      if(codigoconductor == codconductor){
+        idSocketconductorAceptado = codigowsconnection;
+      }else{
+        io.to(codigowsconnection).emit('pasajerorechazo', {item, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
+      }
+    }
+
+    if(idSocketconductorAceptado != ""){ //si encontro el idconnection
+      io.to(idSocketconductorAceptado).emit('pasajeroconfirmo', {objpasajeroSolicitud, createdAt: new Date()}); //envia la confirmacion al conductor
+    }
+
+
+    // //recorriendo los conductores que ofertaron
+    // objpasajeroSolicitud.conductoresofertaron.forEach( (item) => {
+    //     //element.product_desc = element.product_desc.substring(0,10);
+    //   let idsocketconductor = item.propuesta.idsocketconductor.socketidclient;
+    //   if(idsocketconductor != idSocketconductorAceptado){
+    //     io.to(idsocketconductor).emit('pasajerorechazo', {item, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
+    //   }
+    //   console.log("item->>",idsocketconductor)
+    // });
+
+    // // io.emit('pasajeroconfirmo', {objpasajeroSolicitud, createdAt: new Date()});    
+    // io.to(idSocketconductorAceptado).emit('pasajeroconfirmo', {objpasajeroSolicitud, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
+  });
+
+
+  // socket.on('pasajeroconfirma', (objpasajeroSolicitud) => { //aqui el pasajero acepta propuesta de un conductor [version inicial]
+  //   //console.log("objpasajeroSolicitud", objpasajeroSolicitud);
+  //   console.log("<------ pasajero confirma --------------->");
+    
+  //   var idSocketconductorAceptado = objpasajeroSolicitud.clientsocket;
+  //   //recorriendo los conductores que ofertaron
+  //   objpasajeroSolicitud.conductoresofertaron.forEach( (item) => {
+  //       //element.product_desc = element.product_desc.substring(0,10);
+  //     let idsocketconductor = item.propuesta.idsocketconductor.socketidclient;
+  //     if(idsocketconductor != idSocketconductorAceptado){
+  //       io.to(idsocketconductor).emit('pasajerorechazo', {item, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
+  //     }
+  //     console.log("item->>",idsocketconductor)
+  //   });
+
+  //   // io.emit('pasajeroconfirmo', {objpasajeroSolicitud, createdAt: new Date()});    
+  //   io.to(idSocketconductorAceptado).emit('pasajeroconfirmo', {objpasajeroSolicitud, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
+  // });
+
+
+  socket.on('enviomensajeusuario', (objeto) => { //aqui el conductor envia el tracking para todos los usuarios
+    //console.log("solicitó viajee", objetoPasajero);
+    console.log("chat usuario", objeto);
+
+    var idSocketconductorRecepcion = objeto.idsocketreceptor;
+    io.to(idSocketconductorRecepcion).emit('recepcionarmensajedeusuario', {objeto, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
+
+  });
+
+  socket.on('enviomensajeuconductor', (objeto) => { //aqui el conductor envia el tracking para todos los usuarios
+    //console.log("solicitó viajee", objetoPasajero);
+    console.log("chat conductor", objeto);
+    //var idSocketconductorRecepcion = objeto.idsocketreceptor;
+    io.emit('recepcionarmensajedeconductor', {objeto, createdAt: new Date()}); //enviando el idSOCKET al cliente que ingresó
+
+  });
+  
   socket.on('confirmarterminoviaje', (objeto) => { //aqui el conductor envia el tracking para todos los usuarios
     //console.log("solicitó viajee", objetoPasajero);
     var dataconfirmacion = JSON.parse(objeto);
     io.emit('conductorfinalizaviaje', {dataconfirmacion, createdAt: new Date()});    
   });
   
+  // socket.on('loginconductor', (objetoConductor) => {
+  //   //console.log("socket--> ",socket.id);
+  //   //
+  //   let conductoreslogin = getConductoresLogin();
+  //   let cantconductoreslogin = conductoreslogin.length;
+  //   let conductorlogueado = false;
+  //   let objConductor = JSON.parse(objetoConductor);
+  //   //
+  //   //console.log("cantidad->",conductoreslogin.length);
+  //   //
+  //   if(cantconductoreslogin > 0 ){ //si ya hay conductores
+  //     //
+  //     conductorlogueado = busquedaDeConductorLogeado(objConductor.idConductor);
+  //     if(conductorlogueado){
+  //       console.log("conductor logeado-->");
+  //     }else{
+  //       console.log("conductor NO logeado-->");
+  //       // conductoreslogin.push(objConductor);
+  //       // fs.writeFile('dataconductoreslogin.json',  JSON.stringify(conductoreslogin) , 'utf8', function(){
+  //       //   console.log("hola");
+  //       // });
+  //       guardarConductorInFile(objConductor, conductoreslogin);
+  //     }
 
+  //   }else{ //si no hay ningun conductor
+  //     //conductoreslogin.push(objConductor);
+  //     // fs.writeFile('dataconductoreslogin.json',  JSON.stringify(conductoreslogin) , 'utf8', function(){
+  //     //   console.log("hola");
+  //     // });
+  //     guardarConductorInFile(objConductor, conductoreslogin);
+  //   }
 
-  socket.on('loginconductor', (objetoConductor) => {
-    //console.log("socket--> ",socket.id);
-    //
-    let conductoreslogin = getConductoresLogin();
-    let cantconductoreslogin = conductoreslogin.length;
-    let conductorlogueado = false;
-    let objConductor = JSON.parse(objetoConductor);
-    //
-    //console.log("cantidad->",conductoreslogin.length);
-    //
-    if(cantconductoreslogin > 0 ){ //si ya hay conductores
-      //
-      conductorlogueado = busquedaDeConductorLogeado(objConductor.idConductor);
-      if(conductorlogueado){
-        console.log("conductor logeado-->");
-      }else{
-        console.log("conductor NO logeado-->");
-        // conductoreslogin.push(objConductor);
-        // fs.writeFile('dataconductoreslogin.json',  JSON.stringify(conductoreslogin) , 'utf8', function(){
-        //   console.log("hola");
-        // });
-        guardarConductorInFile(objConductor, conductoreslogin);
-      }
-
-    }else{ //si no hay ningun conductor
-      //conductoreslogin.push(objConductor);
-      // fs.writeFile('dataconductoreslogin.json',  JSON.stringify(conductoreslogin) , 'utf8', function(){
-      //   console.log("hola");
-      // });
-      guardarConductorInFile(objConductor, conductoreslogin);
-    }
-
-    //console.log("antes de emitir el mensaje---->");
-    // socket.broadcast.emit('mensajelogin', "se conectó uno");
-    //io.emit('mensajelogin', {user: "WILLIAM CUBBBB", event: 'joined'});    
-  });
+  //   //console.log("antes de emitir el mensaje---->");
+  //   // socket.broadcast.emit('mensajelogin', "se conectó uno");
+  //   //io.emit('mensajelogin', {user: "WILLIAM CUBBBB", event: 'joined'});    
+  // });
 
   socket.on('send-message', (message) => {
     io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});    
@@ -166,6 +247,8 @@ io.on('connection', (socket) => {
     });
   }
 
+
+
   function getConductoresLogin(){
     let conductores = fs.readFileSync('dataconductoreslogin.json');
     let conductoresJSON = JSON.parse(conductores);
@@ -189,6 +272,38 @@ io.on('connection', (socket) => {
     }
     return encontroConductor;
   }
+
+  function setSessionConductor(itemconductor){
+    ARR_CONDUCTORES_ACTIVOS.push(itemconductor);
+  }
+
+  function getSessionConductores(){
+    return ARR_CONDUCTORES_ACTIVOS;
+  }
+
+  function removeitemSessionConductor(idSocketConnection){
+     
+    let encontroid = {
+        encontro : false,
+        posicion : 0,
+        removioitem : false
+    };
+
+   for(let i = 0;i<ARR_CONDUCTORES_ACTIVOS.length; i++){
+       var idconnectsocket = ARR_CONDUCTORES_ACTIVOS[i].idconnection;
+       if(idconnectsocket == idSocketConnection){
+         encontroid.encontro = true;
+         encontroid.posicion = i;
+     }
+   }
+
+   if(encontroid.encontro){ //si encuentra el idconnection entonces eliminar 
+       ARR_CONDUCTORES_ACTIVOS.splice(encontroid.posicion, 1);
+       encontroid.removioitem = true;
+   }
+   return encontroid.removioitem;
+ } 
+
 });
  
 var port = process.env.PORT || 3001;
